ce741c5e6c9c5daf6942a47a6042c3f16d744c97
Anees Shaik <anees.shaik@slu.edu>
2025-02-06 15:17:33 -0600

    message routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 
@@ -11,6 +12,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("server is running on port:" + PORT);
