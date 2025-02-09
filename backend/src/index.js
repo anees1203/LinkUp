a71944a5d7c628e5c5829aeb77f7cb8c517d94d4
Anees Shaik <anees.shaik@slu.edu>
2025-02-08 18:29:55 -0600

    updates the profile pic of user

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,6 +10,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+// Set body size limit 
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(
