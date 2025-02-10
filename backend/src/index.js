310e83fe3635d65114544d615f6f79d462e204ee
Anees Shaik <anees.shaik@slu.edu>
2025-02-09 20:19:45 -0600

    complete

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
-
+const __dirname = path.resolve();
 // Set body size limit 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
@@ -25,6 +25,14 @@ app.use(
 );
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log("server is running on port:" + PORT);
